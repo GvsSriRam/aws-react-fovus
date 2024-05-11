2738ef773d85137370bb601e924e7ad18cfa7441
GVS SRI RAM <gvs.iitkgp@gmail.com>
2024-05-10 22:20:04 -0400

    demo

--- frontend/src/vmFunc.ts
+++ frontend/src/vmFunc.ts
@@ -1,10 +1,9 @@
 import AWS from 'aws-sdk';
-import {EC2Client, RunInstancesCommand} from "@aws-sdk/client-ec2";
-
-const ec2Client = new EC2Client({
+AWS.config.update({
     region: "us-east-1"
-  });
+});
 
+const ec2Client = new AWS.EC2();
 export async function createEC2Instance(id) {
     console.log(id)
 
@@ -13,32 +12,36 @@ export async function createEC2Instance(id) {
     aws s3 cp s3://shell-script-gvs/out.sh ~/out.sh
     cd ~
     chmod 777 out.sh
-    ID="${id}" sh out.sh
+    ID=${id} sh out.sh
   `;
-    console.log(startupScript)
+  console.log(startupScript)
 
-    var Buffer = require('buffer/').Buffer
+let encoder = new TextEncoder();
+let data = encoder.encode(startupScript);
+
+const params = {
+    ImageId: "ami-07caf09b362be10b8",
+    InstanceType: "t2.micro",
+    MinCount: 1,
+    MaxCount: 1,
+    KeyName: "fovus-key",
+    SecurityGroupIds: ["sg-053e4d7d10e8d342a"],
+    IamInstanceProfile: {
+      Name: "ec2-admin",
+    },
+    UserData: btoa(String.fromCharCode.apply(null, data)),
+  };
+
     let instanceId: string | null = null;
-    const command = new RunInstancesCommand({
-        ImageId: "ami-07caf09b362be10b8",
-        InstanceType: "t2.micro",
-        MinCount: 1,
-        MaxCount: 1,
-        KeyName: "fovus-key",
-        SecurityGroupIds: ["sg-053e4d7d10e8d342a"],
-        IamInstanceProfile: {
-          Name: "ec2-admin",
-        },
-        UserData: Buffer.from(startupScript).toString('base64')
-      });
 
-    try {
-        const data = await ec2Client.send(command);
-        console.log("Success", data);
-      } catch (error) {
-        console.error("Error", error);
-      }
-    
+    await ec2Client.runInstances(params).promise()
+        .then(data => {
+            instanceId = data.Instances[0].InstanceId || null;
+            console.log("Created instance", instanceId);
+        })
+        .catch(err => {
+            console.error(err, err.stack);
+        });
 
   return instanceId;
 
